9c9a09ac52b1d7bb98aa517e9daa5c6bcf7778b5
gajalakshmi12 <35587950+gajalakshmi12@users.noreply.github.com>
2025-01-24 17:22:43 -0500

    Update EventualSafeNodes.java

--- 802. Find Eventual Safe States/EventualSafeNodes.java
+++ 802. Find Eventual Safe States/EventualSafeNodes.java
@@ -1,1 +1,30 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> safeNodes = new ArrayList<>();
+        int[] state = new int[graph.length];
 
+        for(int i=0; i<graph.length; i++){
+            if(dfs(graph, i, state)){
+                safeNodes.add(i);
+            }
+        }
+        return safeNodes;
+    }
+
+    private boolean dfs(int[][] graph, int node, int[] state){
+        if(state[node] != 0){
+            return state[node] == 1;
+        }
+
+        state[node] = 2; 
+        for(int neighbor : graph[node]){
+            if(!dfs(graph, neighbor, state)){
+                return false;
+            }
+        }
+
+        state[node] = 1;
+        return true;
+    }
+
+}
